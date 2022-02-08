63bb3713069e6fe7168e1fde77a97a38712abce3
malecks <alex@alexmathers.com>
2022-02-07 18:29:13 -0800

    Add autolayout and refactor

--- src/code.ts
+++ src/code.ts
@@ -1,54 +1,96 @@
-
 import users from './users.json';
 
+const padding:number = 8;
+
 figma.showUI(__html__)
-// Load text style for username labels
-figma.loadFontAsync({ family: "Roboto", style:"Regular" })
+// Load text style for username labels (do this outside of loop)
+figma.loadFontAsync({ 
+  family: "Roboto", style:"Regular" 
+})
 
 figma.ui.onmessage = msg => {
   if (msg.type === 'create-users') {
-    const nodes: SceneNode[] = []
-    for (let i = 0; i < msg.count; i++) {
-      const user = users[i]
-
-      //Add circle with fill
-      const avatar = createAvatar(user)
-      avatar.y = i * 150
-      figma.currentPage.appendChild(avatar)
-     
-      //Add username
-
-      const userName = createUserLabel(user)
-      userName.y = i*150 + 20
-      userName.x = 72
-      figma.currentPage.appendChild(userName)
-
-      //Group objects
-      const group = figma.group([userName, avatar], figma.currentPage)
-      let index = i + 1
-      group.name = "User" + " " + index
-      nodes.push(group)
-    }
-
-    figma.currentPage.selection = nodes
-    figma.viewport.scrollAndZoomIntoView(nodes)
+    createUsers(msg.count);
   }
-  figma.closePlugin()
+
+  figma.closePlugin();
+}
+
+function createUsers(count: number) {
+
+  let nodes: SceneNode[] = [];
+  for (let i = 0; i < count; i++) {
+    let user = users[i];
+
+    //Add circle with fill
+    let avatar = createAvatar(user);
+    avatar.y = i * 150;
+    figma.currentPage.appendChild(avatar);
+   
+    //Add username
+    let userName = createUserLabel(user);
+    userName.y = i*150 + 20;
+    userName.x = 72;
+    figma.currentPage.appendChild(userName);
+
+    //Group with autolayout
+    let frame = createUserFrame();
+    frame.appendChild(avatar);
+    frame.appendChild(userName);
+    
+    let index = i + 1;
+    frame.name = "User" + " " + index;
+    nodes.push(frame);
+  }
+
+  //Group users in autolayout
+  let frame = createLayout();
+  frame.name = "Users";
+  for (let node of nodes) {
+    frame.appendChild(node);
+  }
+
+  //Select and zoom into users
+  figma.currentPage.selection = [frame];
+  figma.viewport.scrollAndZoomIntoView([frame]);
+}
+
+function createLayout() {
+  let frame = figma.createFrame();
+  // let padding = 8;
+  frame.layoutMode = "VERTICAL";
+  frame.itemSpacing = padding;
+  frame.paddingTop = frame.paddingLeft = frame.paddingBottom = frame.paddingRight = padding;
+  frame.primaryAxisSizingMode = "AUTO";
+  frame.counterAxisSizingMode = "AUTO";
+  return frame;
 }
 
 // Create functions
 function createUserLabel(user) {
-  const firstLast = user.firstName + " " + (user.lastName ?? "")
-  const text = figma.createText()
-  text.characters = firstLast
-  text.fontSize = 17
-  return text
+  let firstLast = user.firstName + " " + (user.lastName ?? "");
+  let text = figma.createText();
+  text.characters = firstLast;
+  text.fontSize = 17;
+  return text;
 }
 
 function createAvatar(user) {
-  const ellipse = figma.createEllipse()
-  ellipse.resize(60, 60)
-  ellipse.fills = [{type: 'SOLID', color: {r: 0.85, g: 0.85, b: 0.85}}]
-  ellipse.name = "Avatar"
-  return ellipse
+  let ellipse = figma.createEllipse();
+  ellipse.resize(60, 60);
+  ellipse.fills = [{type: 'SOLID', color: {r: 0.85, g: 0.85, b: 0.85}}];
+  ellipse.name = "Avatar";
+  return ellipse;
+}
+
+function createUserFrame() {
+  let frame = figma.createFrame();
+  frame.layoutMode = "HORIZONTAL";
+  frame.counterAxisAlignItems = "CENTER";
+  frame.paddingLeft = frame.paddingRight = frame.paddingTop = frame.paddingBottom = padding;
+  frame.itemSpacing = padding;
+  frame.primaryAxisSizingMode = "AUTO";
+  frame.counterAxisSizingMode = "AUTO";
+  frame.fills = [];
+  return frame;
 }
